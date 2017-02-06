d38254cd7329b7bd3f8de9975e31fd54107472b9
Mikael Roos <mikael.t.h.roos@gmail.com>
2017-02-06 16:10:06 +0100

    adding example programs on nodejs server

--- example/nodejs/serverUptime/index.js
+++ example/nodejs/serverUptime/index.js
@@ -4,9 +4,9 @@
  */
 "use strict";
 
-import server from "./server.js";
+import server from "./serverUptime.js";
 
 // Start the server to listen on a port
 server.listen(1337);
 
-console.log("Simple server listen on port 1337.");
+console.log("Simple server replying with output of command uptime, listen on port 1337.");
--- example/nodejs/simpleJsonServer/index.js
+++ example/nodejs/simpleJsonServer/index.js
@@ -4,9 +4,9 @@
  */
 "use strict";
 
-import server from "./server.js";
+import server from "./jsonServer.js";
 
 // Start the server to listen on a port
 server.listen(1337);
 
-console.log("Simple server listen on port 1337.");
+console.log("Simple server replying with JSON object listen on port 1337.");
